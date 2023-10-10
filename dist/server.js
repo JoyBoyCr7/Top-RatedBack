3fb911bff084f47e179d24ce630c2f51957388fa
JoyBoyCr7 <Rnyongkah@gmail.com>
2023-10-10 18:11:49 -0500

    auth

--- dist/server.js
+++ dist/server.js
@@ -16,7 +16,7 @@ const DATABASE_URL = process.env.DATABASE_URL;
 // .on('error', (error) => console.log(error));
 // middleware
 app.use(cors({
-    origin: "https://top-rated-front.vercel.app",
+    origin: "http://localhost:5173",
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
     allowedHeaders: ['Content-Type', 'Authorization'],
--- server.ts
+++ server.ts
@@ -24,7 +24,7 @@ const DATABASE_URL = process.env.DATABASE_URL
 
 // middleware
 app.use(cors({
-    origin:"https://top-rated-front.vercel.app",
+    origin:"http://localhost:5173",
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE'], 
     allowedHeaders: ['Content-Type', 'Authorization'],
